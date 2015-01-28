641c8b280de9ba12b46c3a4d224d37c4345e274d
Matthew Conover <mconover@mediamath.com>
2015-01-28 12:14:04 -0500

    added bare adama login file

---
+++ adama.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Adama;
+
+    class Login {
+
+        const user = "";
+        const password = "";
+        const api_key = "";
+        const api_base = "";
+        const login_url = "";
+
+    }
+
+
